e56d30296cf63aad9be1db14ec65abca0d14a89c
Javier Gómez Pereda <gopejavi@gmail.com>
2017-10-26 13:58:39 +0200

    US8: Created more scenarios with weirder inputs but still valid; added DB connector to reset DB or have fixtures so tests are isolated

--- src/test/groovy/CreateAccountSpec.groovy
+++ src/test/groovy/CreateAccountSpec.groovy
@@ -2,10 +2,13 @@ import Classes.SignupData
 import Pages.CreateAccountPage
 import Pages.HomePage
 import geb.spock.GebReportingSpec
+import groovy.sql.Sql
 import spock.lang.Issue
 import spock.lang.Narrative
 import spock.lang.Title
 
+//@Grab('mysql:mysql-connector-java:5.1.25')
+//@GrabConfig(systemClassLoader = true)
 @Title("US8: Create Account")
 @Narrative("""
 As an interested user
@@ -45,7 +48,20 @@ class CreateAccountSpec extends GebReportingSpec {
         and: "I receive an email confirming the account creation"
         true //Note: not implemented as app don't send emails.
 
+        cleanup:
+        println "Restoring Vokuro's DB..."
+        def sql = Sql.newInstance("jdbc:mysql://192.168.99.100?allowMultiQueries=true", "root", "", "com.mysql.cj.jdbc.Driver")
+        sql.execute("use vokuro;" + new File('src/test/groovy/Data/vokuro.sql').text)
+
         where:
-        validSignupData << [new SignupData("gopejavi", "email@mailinator.com", "superSecret", "superSecret")]
+        validSignupData << [
+                new SignupData("gopejavi", "email@mailinator.com", "superSecret", "superSecret"),
+                new SignupData("DOGE", "muchemail@mailinator.com", "soPasswordVerySecretWOW", "soPasswordVerySecretWOW"),
+                new SignupData("E", "eve@mailinator.com", "eveveveve", "eveveveve"),
+                new SignupData(" _some_!·%&/()\"\$WeirdCharsAreAllowedTooAndAlsoLongNamesWOW", "incrediblyLongAndStupidEmailForTheLol@mailinator.com", "superSecret", "superSecret"),
+                new SignupData("Neil DeGrasse", "masterUniverse@mailinator.com", "IamTheGreatMasterOfTheUniverseAndYouKnowIt", "IamTheGreatMasterOfTheUniverseAndYouKnowIt")
+        ]
+
+
     }
 }
